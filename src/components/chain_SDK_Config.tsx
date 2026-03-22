abcc1b7df3b0069f5bb1b090d80ba31b8a874ca5
ManuelPrhyme <manuelbagoole@gmail.com>
2026-03-22 15:29:57 +0300

    f

--- src/components/chain_SDK_Config.tsx
+++ src/components/chain_SDK_Config.tsx
@@ -6,7 +6,7 @@ export const SomniaTestnet = defineChain({
     id:50312,
     name: "Somnia Testnet",
     nativeCurrency:{
-      name:'STT',
+      name:'Somnia Testnet Tokens',
       decimals:18,
       symbol:'STT'
     },
@@ -22,30 +22,17 @@ export const SomniaTestnet = defineChain({
     }
   })
 
-  export const Anvil = defineChain({
-      id:31337,
-      name: "Anvil",
-      nativeCurrency:{
-        name:'ETH',
-        decimals:18,
-        symbol:'ETH'
-      },
-      rpcUrls:{
-        default:
-          {http:['http://127.0.0.1:8545'],
-            webSocket:['ws://127.0.0.1:8545/ws']
-          } 
-      }
-  
-    })
+export const getWalletClient = () => {
+    if (typeof window === 'undefined' || !window.ethereum) throw new Error('No wallet found');
+    return createWalletClient({
+      chain: SomniaTestnet,
+      transport: custom(window.ethereum),
+    });
+  };
 
-    const account = privateKeyToAccount(import.meta.env.VITE_PRIVATE_KEY)
-
-    export const wallet_Client = createWalletClient({
-    account,
-    chain:SomniaTestnet,
-    transport: custom(window.ethereum)
-  })
+export const wallet_Client = new Proxy({} as ReturnType<typeof getWalletClient>, {
+  get: (_, prop) => getWalletClient()[prop as keyof ReturnType<typeof getWalletClient>],
+});
 
   export const public_Client = createPublicClient({
     chain: SomniaTestnet,
@@ -59,8 +46,9 @@ export const SomniaTestnet = defineChain({
 
 
     export const somniaReactSDK = new SDK({
+      public:public_Client,
     wallet:wallet_Client,
-    public:public_Client
+    
   })
 
   export const emitter_handler = "0x177B95B9C7B2A5E8182b12DF8F5e3Fef65506a7e" as `0x${string}`
